9540e0c749cc74c0ebf4925767970ae5a4dcab0a
devxris <chuwei0915@gmail.com>
2017-10-03 10:25:18 +0800

    Simple UI and setup

---
+++ CollectionView/Controller/DoodleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DoodleViewController.swift
+//  CollectionView
+//
+//  Created by Chris Huang on 03/10/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class DoodleViewController: UIViewController {
+
+	private var doodleImages = ["DoodleIcons-1", "DoodleIcons-2", "DoodleIcons-3", "DoodleIcons-4",
+	                            "DoodleIcons-5", "DoodleIcons-6", "DoodleIcons-7", "DoodleIcons-8",
+	                            "DoodleIcons-9", "DoodleIcons-10", "DoodleIcons-11", "DoodleIcons-12",
+	                            "DoodleIcons-13", "DoodleIcons-14", "DoodleIcons-15", "DoodleIcons-16",
+	                            "DoodleIcons-17", "DoodleIcons-18", "DoodleIcons-19", "DoodleIcons-20"]
+
+}
+
